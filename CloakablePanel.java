f222f0ed7c64994d0799198c569b7891daf45c05
Robert Handy <robbyhandy@Roberts-MacBook-Pro.local>
2016-08-19 15:52:15 -0700

    added manual tuning

--- CloakablePanel.java
+++ CloakablePanel.java
@@ -11,7 +11,7 @@ under the MIT license
 This class creates a panel that 
 retains its size when invisible
 
-******************/
+ ******************/
 
 import javax.swing.JPanel;
 import javax.swing.Box;
@@ -23,47 +23,47 @@ import java.awt.Insets;
 
 public class CloakablePanel extends JPanel{
 
-    private Component spacer=null;
-    private Border theborder = null;
-    private int xm,ym;
-    private Boolean iscloaked;
-    
-    CloakablePanel(int extraXMargin, int extraYMargin){
-	xm = extraXMargin;
-	ym = extraYMargin;
-	// 10 for flowlayout
-	iscloaked = false;
-    }
+	private Component spacer=null;
+	private Border theborder = null;
+	private int xm,ym;
+	private Boolean iscloaked;
 
-    public void cloak(){
-	if(iscloaked == false){
-	    iscloaked = true;
-	    theborder = getBorder();
-	    //uncloak();
-	    //Insets insets = getInsets();
-	    //spacer = Box.createRigidArea(new Dimension(getWidth() - insets.left - insets.right -xm, getHeight() - insets.top - insets.bottom -ym));
-	    spacer = Box.createRigidArea(new Dimension(getWidth() - xm, getHeight() - ym));
-	    Component[] components = getComponents();
-	    for(int i=0; i < components.length; i++){
-		components[i].setVisible(false);
-	    }
-	    add(spacer);
-	    setBorder(javax.swing.BorderFactory.createEmptyBorder());
+	CloakablePanel(int extraXMargin, int extraYMargin){
+		xm = extraXMargin;
+		ym = extraYMargin;
+		// 10 for flowlayout
+		iscloaked = false;
 	}
-    }
-    
-    public void uncloak(){
-	if(iscloaked == true){
-	    iscloaked = false;
-	    if(spacer != null)
-		remove(spacer);
-	    Component[] components = getComponents();
-	    for(int i=0; i < components.length; i++){
-		components[i].setVisible(true);
-	    }
-	    if(theborder != null)
-		setBorder(theborder);
+
+	public void cloak(){
+		if(iscloaked == false){
+			iscloaked = true;
+			theborder = getBorder();
+			//uncloak();
+			//Insets insets = getInsets();
+			//spacer = Box.createRigidArea(new Dimension(getWidth() - insets.left - insets.right -xm, getHeight() - insets.top - insets.bottom -ym));
+			spacer = Box.createRigidArea(new Dimension(getWidth() - xm, getHeight() - ym));
+			Component[] components = getComponents();
+			for(int i=0; i < components.length; i++){
+				components[i].setVisible(false);
+			}
+			add(spacer);
+			setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		}
 	}
-    }
+
+	public void uncloak(){
+		if(iscloaked == true){
+			iscloaked = false;
+			if(spacer != null)
+				remove(spacer);
+			Component[] components = getComponents();
+			for(int i=0; i < components.length; i++){
+				components[i].setVisible(true);
+			}
+			if(theborder != null)
+				setBorder(theborder);
+		}
+	}
 
 }
